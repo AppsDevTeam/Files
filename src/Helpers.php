eaa0101b687d95c32a51006c734e334da0e30334
Michal Lohnisky <michal@appsdevteam.com>
2015-03-04 13:18:42 +0100

    readme composer Helpers

---
+++ src/Helpers.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace ADT\Files;
+
+class Helpers extends \Nette\Object
+{
+	/* Maximální délka sloupce originalName */
+	const ORIGINAL_NAME_LEN = 255;
+
+	/* Maximální délka sloupce name */
+	const NAME_LEN = 255;
+
+	/* Délka hashe, který se přidá k názvu souboru, aby nebyl název souboru uhodnutelný */
+	const HASH_LEN = 5;
+
+	/*
+	 * Po kolika znacích bude IDčko rozděleno na stromovou strukturu složek.
+	 * Maximální počet záznamů (souborů + složek) M v jedné složce je dán vztahem
+	 * M = 2 * 10^(ID_SPLIT_LEN)
+	 */
+	const ID_SPLIT_LEN = 3;
+
+	/**
+	 * Zadané jméno souboru zkrátí na maximální délku tak, že pokud je $name
+	 * kratší, vrátí ho nezměněné. Pokud je delší, nejprve zkrátí extension na
+	 * maximálně $maxExtLen znaků a zkracuje řetězec $name od konce před
+	 * extension. Název souboru $name nemusí mít příponu. Předpokládá, že $name
+	 * neobsahuje žádnou cestu, pouze název souboru.
+	 * @param string $name
+	 * @param integer $maxLen
+	 * @param integer $maxExtLen
+	 * @return string
+	 */
+	public static function resizeName($name, $maxLen, $maxExtLen = 10) {
+		if (strlen($name) < $maxLen) {
+			return $name;
+		}
+
+		$pathinfo = pathinfo($name);
+
+		if (! isset($pathinfo['extension'])) {
+			return substr($name, 0, $maxLen);
+		}
+
+		// omezení extension
+		$pathinfo['extension'] = substr($pathinfo['extension'], 0, $maxExtLen);
+
+		return substr($pathinfo['filename'], 0, $maxLen - strlen($pathinfo['extension']) - 1) .'.'. $pathinfo['extension'];
+	}
+
+	/**
+	 * Pro daný ActiveRow (id, originalName) vrátí název a cestu k souboru.
+	 * Adresářová struktura je vytvářena tak, aby nebylo v jednom adresáři přiliš
+	 * mnoho záznamů (souborů a složek).
+	 * @param \Nette\Database\Table\ActiveRow $row
+	 * @return string
+	 */
+	public static function getName(\Nette\Database\Table\ActiveRow $row) {
+		$idPart = implode(DIRECTORY_SEPARATOR, str_split((string)$row->id, static::ID_SPLIT_LEN));
+		$hashPart = \Nette\Utils\Random::generate(static::HASH_LEN);
+		$namePart = static::resizeName($row->originalName, static::NAME_LEN - strlen($idPart) - static::HASH_LEN - 2);
+
+		return $idPart .'_'. $hashPart .'_'. $namePart;
+	}
+
+}
