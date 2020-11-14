0293318c631f7221348a06cbd1d7482328257f56
Tomáš Kudělka <thorewi@gmail.com>
2020-11-14 21:40:27 +0000

    Update IFileEntity.php

--- src/Entities/IFileEntity.php
+++ src/Entities/IFileEntity.php
@@ -1,11 +1,11 @@
 <?php
+
 declare(strict_types=1);
 
 namespace ADT\Files\Entities;
 
 interface IFileEntity
 {
-
 	public function getPath(): string;
 
 	public function setBaseDirectoryPath(string $path): self;
@@ -14,24 +14,25 @@ interface IFileEntity
 
 	public function setBaseDirectoryUrl(string $url): self;
 
-	public function setTemporaryContent(string $content, string $originalName): self;
-
 	public function setTemporaryFile(string $filePath, string $originalName): self;
 
-	public function hasTemporaryFile(): bool;
+	public function getTemporaryFile(): ?string;
 
-	public function saveFile(): self;
+	public function setTemporaryContent(string $content, string $originalName): self;
+
+	public function getTemporaryContent(): ?string;
+
+	public function hasTemporaryFileOrContent(): bool;
 
 	public function setOnAfterSave(callable $callback): self;
 
 	public function getOnAfterSave(): ?callable;
 
-	public function getFilename(): string;
+	public function setFilename(string $filename): self;
 
+	public function getFilename(): string;
+	
 	public function getOriginalName(): string;
 
 	public function getOnAfterDelete(): ?callable;
-
-	public function setOnAfterDelete(callable $onAfterDelete): self;
-
 }
